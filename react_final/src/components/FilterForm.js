0893f415a19e09b6e65585dfa65c0cc5a8f0c242
Rauan <rauanuzakbaeva@gmail.com>
2023-12-17 12:39:21 +0600

    first commit

---
+++ react_final/src/components/FilterForm.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './FilterForm.css';
+
+export default function FilterForm(props) {
+  return (
+    <form className="my-6">
+      <div className="is-grouped">
+        <p className="control">
+          <button
+            id='allbtn'
+            className="button is-fullwidth"
+            type="submit"
+            onClick={props.handleAllSubmit}
+          >
+            I Want Questions from All Categories!
+          </button>
+        </p>
+        <h2 className="my-4 control">OR...</h2>
+        <div className="field is-grouped">
+          <div className="control is-expanded">
+            <div className="select is-fullwidth">
+              <select onChange={props.handleChange} id='select'>
+                {props.allCategories.map((category) => (
+                  <option id="options" key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+          <p className="control">
+            <button
+              id='searchbtn'
+              className="button is-info"
+              type="submit"
+              onClick={props.handleCategorySubmit}
+            >
+              Choose Category
+            </button>
+          </p>
+        </div>
+      </div>
+    </form>
+  )
+}
---
+++ react_final/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <h1 id="white" className="title">ARA Quiz!</h1>
+          <h2 id="yellow" className="subtitle">
+            Test your knowledge of trivia! Select a category or answer questions from all possible categories.
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+}
